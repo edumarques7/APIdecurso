e9eeb214888ddf1eb96c7e1fab8634ccc9b926ae
edumarques7 <marques.barbosa@hotmail.com>
2023-09-15 21:15:55 -0300

    att commit

--- core/configs.py
+++ core/configs.py
@@ -9,7 +9,8 @@ class Settings(BaseSettings):
     """
     
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = "postgresql+asyncpg://postgres:123@localhost:5432/faculdade"
+    DB_URL: str = "sqlite:///./sql_app.db"
+    # DB_URL: str = "postgresql+asyncpg://postgres:123@localhost:5432/faculdade"
     DBBaseModel = declarative_base()
     
     JWT_SECRET: str = 'xLgeXKD02H1ksxBJMyC5csII-eU-g58R9EyIP0N67nw'
